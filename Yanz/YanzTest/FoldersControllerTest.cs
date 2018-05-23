62a637bdcd57a32bce8daa04a35b42fa71e83f9e
Sultan Kadyrkesh <sultankadyr@outlook.com>
2018-05-23 16:56:58 +0600

    fix test

--- Yanz/YanzTest/FoldersControllerTest.cs
+++ Yanz/YanzTest/FoldersControllerTest.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.TestHost;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using Xunit;
+using Yanz;
 using Yanz.Controllers.API;
 using Yanz.Data;
 using Yanz.Models;
@@ -11,6 +15,14 @@ namespace YanzTest
 {
     public class FoldersControllerTest
     {
+        TestServer server;
+        HttpClient client;
+
+        public FoldersControllerTest()
+        {
+            server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            client = server.CreateClient();
+        }
         [Fact]
         public void GetTestWithNull()
         {
