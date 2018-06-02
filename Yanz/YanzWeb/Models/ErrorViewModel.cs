9404af4b59c6eb3309ebf99161dbba40cf228b8a
Sultan Kadyrkesh <sultankadyr@outlook.com>
2018-06-02 16:02:38 +0600

    Asp net core 2.1

---
+++ Yanz/YanzWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YanzWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
